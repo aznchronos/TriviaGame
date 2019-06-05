66d183c5057f9e54eade253b1605b5adab25af17
Andy R <andyr914@gmail.com>
2019-06-05 13:52:29 -0700

    6th commit 'Added in a correct/incorrect function'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,25 +8,37 @@ $(document).ready(function(){
     var imagesWin = ["assets/images/1win.gif", "assets/images/2win.gif", "assets/images/3win.gif", "assets/images/4win.gif", "assets/images/5win.gif"];
     var imagesLose = ["assets/images/1lose.gif", "assets/images/2lose.gif", "assets/images/3lose.gif", "assets/images/4lose.gif", "assets/images/5lose.gif"];
 
-    var questions = [
-        "Question 1", "Question 2"
-    ];
+    var questions = ["Question 1", "Question 2", "Questions 3", "Questions 4", "Questions 5"];
 
     var answersArray = [
+        ["Answer1", "Answer2", "Answer3", "Answer4"],
+        ["Answer1", "Answer2", "Answer3", "Answer4"],
+        ["Answer1", "Answer2", "Answer3", "Answer4"],
         ["Answer1", "Answer2", "Answer3", "Answer4"],
         ["Answer1", "Answer2", "Answer3", "Answer4"]
     ];
 
-    var correctAnswersArray = [
-        "Answer2",
-        "Answer3"
-    ];
+    var correctAnswersArray = ["Answer2", "Answer3", "Answer1", "Answer1", "Answer1"];
 
     //To hide the start button
     $("#startGame").on("click", function (event) {
         $("#startGame").hide();
+        $("#image-holder").hide();
         clock();
         generatePage();
+    });
+
+    $("body").on("click", ".answer", function(event){
+        // Attempting to push value of button into choice array
+        choice = $(this).text();
+        if (choice === correctAnswersArray[counter]){
+            correctAnswer();
+            // console.log("Correct count: " + correct)
+
+        } else if(choice !== correctAnswersArray[counter]) {
+            incorrectAnswer();
+            // console.log("Incorrect count: " + incorrect)
+        }
     });
 
     //Set 30 second timer to function;
@@ -48,24 +60,44 @@ $(document).ready(function(){
     function generatePage() {
         gamePage = "<h1 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
             timer + "</span></h1><h3 class='text-center'>" + questions[counter] +
-            "</h3><div class='container qButtons'><button class='answer'>1. " + answersArray[counter][0] +
-            "</button><button class='answer'>2. " + answersArray[counter][1] +
-            "</button><button class='answer'>3. " + answersArray[counter][2] +
-            "</button><button class='answer'>4. " + answersArray[counter][3] +
+            "</h3><div class='container qButtons'><button class='answer'>" + answersArray[counter][0] +
+            "</button><button class='answer'>" + answersArray[counter][1] +
+            "</button><button class='answer'>" + answersArray[counter][2] +
+            "</button><button class='answer'>" + answersArray[counter][3] +
             "</button></div>";
         $(".gameID").html(gamePage);
     };
 
     function correctAnswer(){
-        console.log("correct")
+        correct++;
+        console.log("correct answer: " + correctAnswersArray[counter]);
+        gamePage = "<h1 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timer + "</span></h1><h3 class='text-center'>You Win!</h3>" +
+            "<h3 class='text-center'>The answer is: " + correctAnswersArray[counter] + "</h3>" +
+            "<div id='image-holder' class='text-center'></div>"
+            displayWinImage();
+            $(".gameID").html(gamePage);
+            // setTimeout(wait, 5000);
+    }
+
+    function incorrectAnswer(){
+        incorrect++;
+        console.log("correct answer: " + correctAnswersArray[counter]);
+        gamePage = "<h1 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timer + "</span></h1><h3 class='text-center'>You Lose!</h3>" +
+            "<h3 class='text-center'>The answer is: " + correctAnswersArray[counter] + "</h3>" +
+            "<div id='image-holder' class='text-center'></div>"
+            displayLoseImage();
+            $(".gameID").html(gamePage);
+            // setTimeout(wait, 5000);
     }
 
     function questionTimeout() {
         unanswered++;
         gamePage = "<h1 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
             timer + "</span></h1><h3 class='text-center'>Times Up!</h3>" +
-            "<h3>The answer is: " + correctAnswersArray[counter] + "</h3>" +
-            "<div id='image-holder' class='text-center'>" + "</div>"
+            "<h3 class='text-center'>The answer is: " + correctAnswersArray[counter] + "</h3>" +
+            "<div id='image-holder' class='text-center'></div>"
             displayLoseImage();
             $(".gameID").html(gamePage);
             // setTimeout(wait, 5000);
@@ -73,12 +105,10 @@ $(document).ready(function(){
     
     function displayLoseImage() {
         $("#image-holder").html("<img src=" + imagesLose[counter] + " width='400px'>");
+        $("#image-holder").show();
     }
     function displayWinImage() {
         $("#image-holder").html("<img src=" + imagesWin[counter] + " width='400px'>");
+        $("#image-holder").show();
     }
-
-    // $("body").on("click", "#answer", function(event){
-
-    // });
 });
