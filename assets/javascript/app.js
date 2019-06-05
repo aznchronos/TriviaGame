2ecbc0638b99afe6e500b2c97009fb56e5795074
Andy R <andyr914@gmail.com>
2019-06-05 12:38:47 -0700

    5th commit 'Added images to program, but having problems displaying them during results pages. Will look into other materials to try to resolve this'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
     var unanswered = 0;
     var counter = 0;
     var time = 0;
-    var timer = 30;
+    var timer = 5;
+    var imagesWin = ["assets/images/1win.gif", "assets/images/2win.gif", "assets/images/3win.gif", "assets/images/4win.gif", "assets/images/5win.gif"];
+    var imagesLose = ["assets/images/1lose.gif", "assets/images/2lose.gif", "assets/images/3lose.gif", "assets/images/4lose.gif", "assets/images/5lose.gif"];
 
     var questions = [
         "Question 1", "Question 2"
@@ -33,7 +35,7 @@ $(document).ready(function(){
         function thirty() {
             if (timer === 0) {
                 clearInterval(time);
-                // questionTimer();
+                questionTimeout();
             }
             if (timer > 0) {
                 timer--;
@@ -53,8 +55,26 @@ $(document).ready(function(){
             "</button></div>";
         $(".gameID").html(gamePage);
     };
+
+    function questionTimeout() {
+        unanswered++;
+        gamePage = "<h1 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timer + "</span></h1><h3 class='text-center'>Times Up!</h3>" +
+            "<h3>The answer is: " + correctAnswers[counter] + "</h3>" +
+            "<div id='image-holder' class='text-center'>" + "</div>"
+            displayLoseImage();
+            $(".gameID").html(gamePage);
+            // setTimeout(wait, 5000);
+    };
+    
+    function displayLoseImage() {
+        $("#image-holder").html("<img src=" + imagesLose[counter] + " width='400px'>");
+    }
+    function displayWinImage() {
+        $("#image-holder").html("<img src=" + imagesWin[counter] + " width='400px'>");
+    }
+
     // $("body").on("click", "#answer", function(event){
 
     // });
-
 });
